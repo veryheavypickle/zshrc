f275aff3eb290d1601f08786ab9a76cb8e0bfc6e
veryheavypickle <xavier.carroll01@gmail.com>
2022-09-14 05:41:51 +0200

    Added homebrew installation

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Restart terminal
+zsh
+
 git config --global user.name "veryheavypickle"
 git config --global user.email xavier.carroll01@gmail.com
