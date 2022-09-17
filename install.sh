72044928dce012551d779f9dc2efa3846595f929
veryheavypickle <xavier.carroll01@gmail.com>
2022-09-17 15:47:10 +0200

    Added python install

--- install.sh
+++ install.sh
@@ -5,5 +5,10 @@
 # Restart terminal
 zsh
 
+# install python
+brew install pyenv
+pyenv install 3.10.6
+pyenv global 3.10.6
+
 git config --global user.name "veryheavypickle"
 git config --global user.email xavier.carroll01@gmail.com
